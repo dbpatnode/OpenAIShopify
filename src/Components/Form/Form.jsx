00a056376257825cf80a8570244892973052abe0
Daniel Patnode <dbpatnode@gmail.com>
2022-05-19 15:20:30 -0700

    moved engines data to data folder

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,3 +1,5 @@
+import { engines } from '../../Data/engines';
+
 const Form = ({
   handleSubmit,
   userPrompt,
@@ -5,29 +7,6 @@ const Form = ({
   AIEngine,
   setAIEngine,
 }) => {
-  const engines = [
-    {
-      engineName: 'text-curie-001',
-      engineDescription:
-        'Very capable, but faster and lower cost than Davinci.',
-    },
-    {
-      engineName: 'text-davinci-002',
-      engineDescription:
-        'Most capable GPT-3 model. Can do any task the other models can do, often with less context. In addition to responding to prompts, also supports inserting completions within text.',
-    },
-    {
-      engineName: 'text-babbage-001',
-      engineDescription:
-        'Capable of straightforward tasks, very fast, and lower cost.',
-    },
-    {
-      engineName: 'text-ada-001',
-      engineDescription:
-        '	Capable of very simple tasks, usually the fastest model in the GPT-3 series, and lowest cost.',
-    },
-  ];
-
   const handleChange = (e) => {
     const { value } = e.target;
     setUserPrompt(value);
@@ -39,7 +18,6 @@ const Form = ({
     setAIEngine(selectedEngine);
   };
 
-  console.log(userPrompt);
   return (
     <div className='Form'>
       <form onSubmit={(e) => handleSubmit(e)}>
