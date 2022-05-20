904748ba9ca0d6ad36618379279092bb9f60aa2e
Daniel Patnode <dbpatnode@gmail.com>
2022-05-19 17:08:43 -0700

    first of type of respone has a gradient leading into the grey

--- src/Components/Responses/Responses.jsx
+++ src/Components/Responses/Responses.jsx
@@ -1,36 +1,36 @@
 const Responses = ({ responses }) => {
   return (
-    <div>
+    <div className='Responses-container'>
       <h3>Responses</h3>
       {responses?.map((resp, index) => {
         const { response, prompt, engine } = resp;
 
         return (
           <div className='Response' key={index}>
-            <div>
+            <span>
               <p>
                 <span className='prompt-span'>
                   <b>Prompt:</b>
                 </span>
                 <span>{prompt}</span>
               </p>
-            </div>
-            <div>
+            </span>
+            <span>
               <p>
                 <span className='response-span'>
                   <b>Response:</b>
                 </span>
                 <span>{response}</span>
               </p>
-            </div>
-            <div>
+            </span>
+            <span>
               <p>
                 <span className='engine-span'>
                   <b>Engine:</b>
                 </span>
                 <span>{engine}</span>
               </p>
-            </div>
+            </span>
           </div>
         );
       })}
