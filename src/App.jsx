2cd17b81dbda8d51b35ae5cf49ea84963f92c678
Daniel Patnode <dbpatnode@gmail.com>
2022-05-21 21:30:43 -0700

    unused useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from 'react';
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './Components/Nav/Nav';
 import Home from './Components/Home/Home';
 import AIContainer from './Components/AIContainer/AIContainer';
 
 function App() {
-  // let navigate = useNavigate();
-
   const [routes] = useState([
     {
       path: '',
@@ -19,7 +17,7 @@ function App() {
       linkText: 'Fun With AI',
     },
   ]);
-  // console.log(navigate);
+
   return (
     <div className='App'>
       <BrowserRouter>
