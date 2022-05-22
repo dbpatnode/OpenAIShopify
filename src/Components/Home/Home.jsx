a5c388f63f0788576889bc82b026496c489463fe
Daniel Patnode <dbpatnode@gmail.com>
2022-05-21 18:00:13 -0700

    general layout for home and nav

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,13 @@
 const Home = () => {
-  return <div className='Home'>home container</div>;
+  return (
+    <div className='Home w-70'>
+      <div>
+        <header>
+          <h1>home container</h1>
+        </header>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
